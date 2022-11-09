5286b8a79e0b45163fde363abe2d6dad3f4f31f7
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-08 21:29:11 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -1,9 +1,18 @@
 export class SignUpController {
-  // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line consistent-return
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
-    };
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
   }
 }
