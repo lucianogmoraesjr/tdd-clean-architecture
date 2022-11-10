48dcb229d0f925e95102065f71b9d846d2bc1829
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-10 17:35:56 -0300

    test: ensure SignUpController returns 500 if CreateAccount throws exception

--- src/presentation/controllers/sign-up/sign-up.spec.ts
+++ src/presentation/controllers/sign-up/sign-up.spec.ts
@@ -233,4 +233,26 @@ describe('SignUp Controller', () => {
       passwordConfirmation: 'any_password',
     });
   });
+
+  test('Should be able to return 500 if CreateAccount throws exception', () => {
+    const { sut, createAccountStub } = makeSut();
+
+    jest.spyOn(createAccountStub, 'execute').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
 });
