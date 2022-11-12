61d1c371cc6c96e6e546e398b4ddd1e4cb5a34d2
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-12 17:02:54 -0300

    refactor: move suts creation to yout own factory method helper

--- src/data/use-cases/create-account/db-create-account.spec.ts
+++ src/data/use-cases/create-account/db-create-account.spec.ts
@@ -1,16 +1,34 @@
+import { Encrypter } from '../../protocols/encrypter';
 import { DbCreateAccount } from './db-create-account';
+
+interface SutTypes {
+  sut: DbCreateAccount;
+  encrypterStub: Encrypter;
+}
+
+const makeEncrypterStub = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return Promise.resolve('hashed_password');
+    }
+  }
+
+  return new EncrypterStub();
+};
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypterStub();
+  const sut = new DbCreateAccount(encrypterStub);
+
+  return {
+    sut,
+    encrypterStub,
+  };
+};
 
 describe('DbCreateAccount UseCase', () => {
   test('Should be able to call Encrypter with correct password', () => {
-    class EncrypterStub {
-      async encrypt(value: string): Promise<string> {
-        return Promise.resolve('hashed_password');
-      }
-    }
-
-    const encrypterStub = new EncrypterStub();
-
-    const sut = new DbCreateAccount(encrypterStub);
+    const { encrypterStub, sut } = makeSut();
 
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
 
