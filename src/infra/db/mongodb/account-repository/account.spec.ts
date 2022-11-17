2124a5d5a30b40a2479d646ef261a9252d415baa
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-17 17:17:34 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,34 @@
+import MongoHelper from '../helpers/mongo-helper';
+
+import { AccountMongoRepository } from './account';
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should be able to return an account on success', async () => {
+    const sut = new AccountMongoRepository();
+
+    const account = await sut.execute({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+    });
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email');
+    expect(account.password).toBe('any_password');
+  });
+});
