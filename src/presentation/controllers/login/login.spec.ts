c946546816fd3da2fca72888a73d04ffcb8949ad
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-22 19:04:11 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -53,7 +53,7 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')));
   });
 
-  test('Should be able to return 400 if no email is provided', async () => {
+  test('Should be able to return 400 if no password is provided', async () => {
     const { sut } = makeSut();
 
     const httpRequest = {
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -7,11 +7,13 @@ export class LoginController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
 
   handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) {
+    const { email, password } = httpRequest.body;
+
+    if (!email) {
       return Promise.resolve(badRequest(new MissingParamError('email')));
     }
 
-    if (!httpRequest.body.password) {
+    if (!password) {
       return Promise.resolve(badRequest(new MissingParamError('password')));
     }
 
