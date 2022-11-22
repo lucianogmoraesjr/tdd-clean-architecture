e620e398b3eabaa1e916cb9863ae9c815d2853ac
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-22 19:38:14 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws exception

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,31 +1,35 @@
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest } from '../../helpers/http-helper';
+import { badRequest, serverError } from '../../helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { EmailValidator } from '../sign-up/sign-up-protocols';
 
 export class LoginController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
 
-  handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body;
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { email, password } = httpRequest.body;
 
-    if (!email) {
-      return Promise.resolve(badRequest(new MissingParamError('email')));
+      if (!email) {
+        return Promise.resolve(badRequest(new MissingParamError('email')));
+      }
+
+      if (!password) {
+        return Promise.resolve(badRequest(new MissingParamError('password')));
+      }
+
+      const isEmailValid = this.emailValidator.isValid(httpRequest.body.email);
+
+      if (!isEmailValid) {
+        return Promise.resolve(badRequest(new InvalidParamError('email')));
+      }
+
+      return Promise.resolve({
+        statusCode: 200,
+        body: 'ok',
+      });
+    } catch (error) {
+      return serverError(error as Error);
     }
-
-    if (!password) {
-      return Promise.resolve(badRequest(new MissingParamError('password')));
-    }
-
-    const isEmailValid = this.emailValidator.isValid(httpRequest.body.email);
-
-    if (!isEmailValid) {
-      return Promise.resolve(badRequest(new InvalidParamError('email')));
-    }
-
-    return Promise.resolve({
-      statusCode: 200,
-      body: 'ok',
-    });
   }
 }
