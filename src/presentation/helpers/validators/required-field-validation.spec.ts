48e39e82433c9c2f7fda0d1dd7c6cb0ad9c0e3cb
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-23 16:43:51 -0300

    test: ensure RequiredFieldValidation not returns if validation pass

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -4,7 +4,13 @@ import { RequiredFieldValidation } from './required-field-validation';
 describe('RequiredField Validation', () => {
   test('Should be able to return a MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('field');
-    const error = sut.validate({ name: 'any_name' });
+    const error = sut.validate({ name: 'any_value' });
     expect(error).toEqual(new MissingParamError('field'));
+  });
+
+  test('Should not be able to return if validation pass', () => {
+    const sut = new RequiredFieldValidation('field');
+    const error = sut.validate({ field: 'any_value' });
+    expect(error).toBeFalsy();
   });
 });
