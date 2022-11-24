dbc5ff335d4fd31bf00c023205ee30e2758ac5b5
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-24 09:07:01 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,10 +1,13 @@
 import { CreateAccountRepository } from '../../../../data/protocols/db/create-account-repository';
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository';
 import { Account } from '../../../../domain/entities/account';
 import { CreateAccountDTO } from '../../../../domain/use-cases/create-account';
 
 import MongoHelper from '../helpers/mongo-helper';
 
-export class AccountMongoRepository implements CreateAccountRepository {
+export class AccountMongoRepository
+  implements CreateAccountRepository, LoadAccountByEmailRepository
+{
   async create(account: CreateAccountDTO): Promise<Account> {
     const accountCollection = MongoHelper.getCollection('accounts');
 
@@ -15,5 +18,23 @@ export class AccountMongoRepository implements CreateAccountRepository {
     const newAccount = { ...rest, id: _id.toString() };
 
     return newAccount;
+  }
+
+  async loadByEmail(email: string): Promise<Account | null> {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    const result = await accountCollection.findOne({ email });
+
+    if (!result) {
+      return null;
+    }
+
+    const { _id, ...rest } = result as any;
+
+    const account: Account = {
+      ...rest,
+      id: _id.toString(),
+    };
+
+    return account;
   }
 }
