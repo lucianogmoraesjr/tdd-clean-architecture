e6b23abfe133801908ec49c8ba16ebc571637adb
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-24 16:16:24 -0300

    test: add login route

---
+++ src/main/routes/login/login-routes.test.ts
@@ -0,0 +1,36 @@
+import { hash } from 'bcrypt';
+import { Collection } from 'mongodb';
+import request from 'supertest';
+import MongoHelper from '../../../infra/db/mongodb/helpers/mongo-helper';
+import app from '../../config/app';
+
+let accountCollection: Collection;
+
+describe('Login Routes', () => {
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should be able to login an existing account', async () => {
+    const hashedPassword = await hash('123', 12);
+
+    await accountCollection.insertOne({
+      name: 'John',
+      email: 'john@mail.com',
+      password: hashedPassword,
+    });
+
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'john@mail.com',
+        password: '123',
+      })
+      .expect(200);
+  });
+});
