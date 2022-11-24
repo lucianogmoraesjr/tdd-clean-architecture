4da8c2776cbaad8c61eda1d5f8b1e54753bda24b
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-24 09:07:25 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,10 +1,13 @@
+import { Collection } from 'mongodb';
 import MongoHelper from '../helpers/mongo-helper';
 
 import { AccountMongoRepository } from './account';
 
+let accountCollection: Collection;
+
 describe('Account Mongo Repository', () => {
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts');
+    accountCollection = MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
   });
 
@@ -12,19 +15,45 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect();
   });
 
-  test('Should be able to return an account on success', async () => {
+  test('Should be able to return an account on create success', async () => {
     const sut = new AccountMongoRepository();
 
     const account = await sut.create({
       name: 'any_name',
-      email: 'any_email',
+      email: 'any_email@mail.com',
       password: 'any_password',
     });
 
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
     expect(account.name).toBe('any_name');
-    expect(account.email).toBe('any_email');
+    expect(account.email).toBe('any_email@mail.com');
     expect(account.password).toBe('any_password');
+  });
+
+  test('Should be able to return an account on loadByEmail success', async () => {
+    const sut = new AccountMongoRepository();
+
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+
+    const account = await sut.loadByEmail('any_email@mail.com');
+
+    expect(account).toBeTruthy();
+    expect(account?.id).toBeTruthy();
+    expect(account?.name).toBe('any_name');
+    expect(account?.email).toBe('any_email@mail.com');
+    expect(account?.password).toBe('any_password');
+  });
+
+  test('Should be able to return null if loadByEmail fails', async () => {
+    const sut = new AccountMongoRepository();
+
+    const account = await sut.loadByEmail('any_email@mail.com');
+
+    expect(account).toBeFalsy();
   });
 });
