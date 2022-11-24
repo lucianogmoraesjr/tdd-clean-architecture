db9871f2f0d6a432ad5e99981a7c9e27c082cfca
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-23 21:20:54 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/use-cases/authentication/db-authentication.ts
+++ src/data/use-cases/authentication/db-authentication.ts
@@ -5,12 +5,14 @@ import {
 import { HashComparer } from '../../protocols/cryptography/hash-comparer';
 import { TokenGenerator } from '../../protocols/cryptography/token-generator';
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository';
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
     private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository,
   ) {}
 
   async execute(data: AuthenticationDTO): Promise<string | null> {
@@ -24,6 +26,7 @@ export class DbAuthentication implements Authentication {
 
       if (isValid) {
         const accessToken = await this.tokenGenerator.execute(account.id);
+        await this.updateAccessTokenRepository.execute(account.id, accessToken);
         return accessToken;
       }
     }
