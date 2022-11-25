af1fb0442d9ef0c913d2f2551ac7537323353305
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-24 22:22:51 -0300

    feat: ensure DbCreateAccount calls LoadAccountByEmailRepository with correct value

--- src/data/use-cases/create-account/db-create-account-protocols.ts
+++ src/data/use-cases/create-account/db-create-account-protocols.ts
@@ -2,3 +2,4 @@ export * from '../../../domain/entities/account';
 export * from '../../../domain/use-cases/create-account';
 export * from '../../protocols/cryptography/hasher';
 export * from '../../protocols/db/account/create-account-repository';
+export * from '../../protocols/db/account/load-account-by-email-repository';
--- src/data/use-cases/create-account/db-create-account.ts
+++ src/data/use-cases/create-account/db-create-account.ts
@@ -4,15 +4,18 @@ import {
   CreateAccountDTO,
   Account,
   CreateAccountRepository,
+  LoadAccountByEmailRepository,
 } from './db-create-account-protocols';
 
 export class DbCreateAccount implements CreateAccount {
   constructor(
     private readonly hasher: Hasher,
     private readonly createAccountRepository: CreateAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
   ) {}
 
   async execute(accountData: CreateAccountDTO): Promise<Account> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email);
     const hashedPassword = await this.hasher.hash(accountData.password);
 
     const newAccount = { ...accountData, password: hashedPassword };
