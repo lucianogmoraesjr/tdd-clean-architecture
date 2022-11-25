2523b8e4d6f5a2a0d25fba27a6bc3c88a876c8fa
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-25 09:16:05 -0300

    test: ensure DbCreateSurvey calls CreateSurveyRepository with correct values

---
+++ src/data/use-cases/create-survey/db-create-survey.spec.ts
@@ -0,0 +1,52 @@
+import { DbCreateSurvey } from './db-create-survey';
+import {
+  CreateSurveyDTO,
+  CreateSurveyRepository,
+} from './db-create-survey-protocols';
+
+const makeFakeSurveyData = (): CreateSurveyDTO => ({
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer',
+    },
+  ],
+});
+
+const makeCreateSurveyRepositoryStub = (): CreateSurveyRepository => {
+  class CreateSurveyRepositoryStub implements CreateSurveyRepository {
+    async create(surveyData: CreateSurveyDTO): Promise<void> {
+      return Promise.resolve();
+    }
+  }
+
+  return new CreateSurveyRepositoryStub();
+};
+
+interface SutTypes {
+  sut: DbCreateSurvey;
+  createSurveyRepository: CreateSurveyRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const createSurveyRepository = makeCreateSurveyRepositoryStub();
+  const sut = new DbCreateSurvey(createSurveyRepository);
+
+  return {
+    sut,
+    createSurveyRepository,
+  };
+};
+
+describe('DbCreateSurvey UseCase', () => {
+  test('Should be able to call CreateSurveyRepository with correct values', async () => {
+    const { sut, createSurveyRepository } = makeSut();
+
+    const createSpy = jest.spyOn(createSurveyRepository, 'create');
+
+    await sut.create(makeFakeSurveyData());
+
+    expect(createSpy).toHaveBeenCalledWith(makeFakeSurveyData());
+  });
+});
