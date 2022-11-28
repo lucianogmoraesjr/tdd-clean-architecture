d6f4e63c1959998511beb6e86b8b06b286bab005
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-28 08:12:01 -0300

    test: ensure DbListSurveys calls ListSurveysRepository

---
+++ src/data/use-cases/list-surveys/db-list-surveys.spec.ts
@@ -0,0 +1,64 @@
+import { Survey } from '../../../domain/entities/survey';
+import { ListSurveysRepository } from '../../protocols/db/survey/list-surveys-repository';
+import { DbListSurveys } from './db-list-surveys';
+
+const makeFakeSurveys = (): Survey[] => [
+  {
+    id: 'any_id',
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer',
+      },
+    ],
+    date: new Date(),
+  },
+  {
+    id: 'other_id',
+    question: 'other_question',
+    answers: [
+      {
+        image: 'other_image',
+        answer: 'other_answer',
+      },
+    ],
+    date: new Date(),
+  },
+];
+
+const makeListSurveysRepository = (): ListSurveysRepository => {
+  class ListSurveysRepositoryStub implements ListSurveysRepository {
+    async list(): Promise<Survey[] | undefined> {
+      return Promise.resolve(makeFakeSurveys());
+    }
+  }
+
+  return new ListSurveysRepositoryStub();
+};
+
+interface SutTypes {
+  sut: DbListSurveys;
+  listSurveysRepositoryStub: ListSurveysRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const listSurveysRepositoryStub = makeListSurveysRepository();
+  const sut = new DbListSurveys(listSurveysRepositoryStub);
+
+  return {
+    sut,
+    listSurveysRepositoryStub,
+  };
+};
+
+describe('DbListSurveys', () => {
+  test('Should be able to call ListSurveys', async () => {
+    const { sut, listSurveysRepositoryStub } = makeSut();
+    const listSpy = jest.spyOn(listSurveysRepositoryStub, 'list');
+
+    await sut.list();
+
+    expect(listSpy).toHaveBeenCalled();
+  });
+});
