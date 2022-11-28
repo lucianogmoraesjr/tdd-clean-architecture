fe04ca2df482c48172aba227b3101ca9eb174c12
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-11-28 08:39:48 -0300

    feat: ensure SurveyMongoRepository list surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,12 +1,36 @@
+import { ListSurveysRepository } from '../../../../data/protocols/db/survey/list-surveys-repository';
 import {
   CreateSurveyDTO,
   CreateSurveyRepository,
 } from '../../../../data/use-cases/create-survey/db-create-survey-protocols';
+import { Survey } from '../../../../domain/entities/survey';
 import MongoHelper from '../helpers/mongo-helper';
 
-export class SurveyMongoRepository implements CreateSurveyRepository {
+export class SurveyMongoRepository
+  implements CreateSurveyRepository, ListSurveysRepository
+{
   async create(surveyData: CreateSurveyDTO): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.insertOne(surveyData);
+  }
+
+  async list(): Promise<Survey[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    const surveysMongoResult = await surveyCollection.find().toArray();
+
+    const surveys: Survey[] = [];
+
+    for (const survey of surveysMongoResult) {
+      const { _id, ...rest } = survey as any;
+
+      const mappedSurvey: Survey = {
+        ...rest,
+        id: _id.toString(),
+      };
+
+      surveys.push(mappedSurvey);
+    }
+
+    return surveys;
   }
 }
